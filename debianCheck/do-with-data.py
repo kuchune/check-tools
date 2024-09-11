6e1d018afa67e4b7a11431ea94cfe822ff21286e
kuchune <kuchune@uniontech.com>
2024-09-11 18:10:38 +0800

    fix: update keys set

--- debianCheck/do-with-data.py
+++ debianCheck/do-with-data.py
@@ -1,7 +1,18 @@
-import sys
+
 import json
 import os
 
+# check_type = os.environ.get("check_type", "all")
+# check_keys = os.environ.get("check_keys", "export,unset")
+repo_name = os.environ.get("repo_name", "reviews-team-test/test_jenkins")
+pull_number = os.environ.get("pull_number", "3")
+api_token = os.environ.get("api_token")
+
+# key_list = check_keys.split(',') #关键字以','号分隔
+keyJson = {
+  "modify": "getcap,setcap,lshw,dmidecode",
+  "all": "export,unset"
+}
 # 在增加和修改内容中筛选敏感词
 # checkType: 1, 在增加和修改内容筛选敏感词
 def filter_keys_in_modify(content, keyLst):
@@ -37,39 +48,58 @@ def filter_keys_in_all(content, keyLst):
     return strJson
 
 
-def filter_keywords(content_dict, keyLst, checkType):
+def filter_keywords(content_dict, keyJson):
+    NoNeedSuffix = [".js", ".vue", ".ts", ".less", ".html", ".go", ".css", ".json", ".txt", ".doc", ".jpg", ".png", ".svg", ".py", '.yml', '.md', '.sha1', '.log']
     originInfo = {}
     resultInfo = {}
+    isPass = True
     if content_dict:
         for fileTemp in content_dict:
+            noNeed = False
+            filename = fileTemp['filename']
+            for endString in NoNeedSuffix:
+                if filename.endswith(endString):
+                    noNeed = True
+                    break
+            if noNeed:
+                continue
             originInfo[fileTemp['filename']] = {
                 "a": [],
                 "b": []
             }
-            filePatch = fileTemp['patch']
-            fileContent = filePatch.splitlines()
-            for line in fileContent:
-                if line.startswith("-"):
-                    originInfo[fileTemp['filename']]["a"].append(line.lstrip("-"))
-                elif line.startswith("+"):
-                    originInfo[fileTemp['filename']]["b"].append(line.lstrip("+"))
-
-        if checkType == 'modify':
-            resultInfo = filter_keys_in_modify(originInfo, keyLst)
-        elif checkType == 'all':
-            resultInfo = filter_keys_in_all(originInfo, keyLst)
-        else:
-            print("异常类型")
-            exit(1)
+            if 'patch' in fileTemp.keys():
+                filePatch = fileTemp['patch']
+                fileContent = filePatch.splitlines()
+                for line in fileContent:
+                    if line.startswith("-"):
+                        originInfo[fileTemp['filename']]["a"].append(line.lstrip("-"))
+                    elif line.startswith("+"):
+                        originInfo[fileTemp['filename']]["b"].append(line.lstrip("+"))
+        
+        for checkType in keyJson:
+            keyLst = keyJson[checkType].split(',') #关键字以','号分隔
+            # resultInfo = filter_keywords(pfInfo, key_list, check_type)
+            if checkType == 'modify':
+                resultInfo = filter_keys_in_modify(originInfo, keyLst)
+            elif checkType == 'all':
+                resultInfo = filter_keys_in_all(originInfo, keyLst)
+            else:
+                print("异常类型")
+                # exit(1)
+            if resultInfo:
+                isPass = False
+                writeJson(resultInfo, f'result-{checkType}.json')
     else:
         print("原始解析数据为空")
-           
-    if resultInfo:
-      print(f"[FAIL]: 敏感词检查不通过{list(resultInfo.keys())}")
-      writeJson(resultInfo, 'result.json')
-      exit(1)
-    else:
-      print(f"[PASS]: 敏感词{checkKeys}检查通过")
+    
+    # if resultInfo:
+    #     isPass = False
+    # #   print(f"[FAIL]: 敏感词检查不通过{list(resultInfo.keys())}")
+    #     writeJson(resultInfo, 'result.json')
+    #   exit(1)
+    # else:
+    #   print(f"[PASS]: 敏感词{checkKeys}检查通过")
+    return isPass
 
 
 # 读取json文件
@@ -80,20 +110,27 @@ def readJson(filepath):
             data = json.load(file)
     return data
 
-
+# 写Json文件
 def writeJson(originInfo, logFile):
     with open(logFile, "w+") as fout:
         if isinstance(originInfo, dict):
             fout.write(json.dumps(originInfo, indent=4, ensure_ascii=False))
 
+# 获取pr中变更文件信息
+def get_pulls_files():
+    import requests
+    url = f'https://api.github.com/repos/{repo_name}/pulls/{pull_number}/files'
+    # print(f'apiurl is {url}')
+    headers = {
+        "Authorization": f"Bearer {api_token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+        "Accept": "application/vnd.github+json"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+      return response.json()
+    else:
+      print(response.status_code)
+      print(f"获取{url}失败, 错误信息：", response.text)
 
-if __name__ == '__main__':
-    checkType = sys.argv[1]
-    global checkKeys 
-    checkKeys = sys.argv[2]
-    jsonSource = sys.argv[3] # json文件路径
-    
-    key_list = checkKeys.split(',') #关键字以','号分隔
-    content_dict = readJson(jsonSource)
-
-    filter_keywords(content_dict, key_list, checkType)
+print(str(filter_keywords(get_pulls_files(), keyJson)))
